3fc29730ff40c738c56aebb8fc8650f5b11df2fa
VozzRoman <looper0500@ukr.net>
2023-04-14 18:08:00 +0300

    adding

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
+const {DB_HOTS} = require('./config');
 
-const DB_HOTS ='mongodb+srv://Roman:rKBWkc4QPlDtBiWT@cluster0.jwv1esx.mongodb.net/contacts__reader?retryWrites=true&w=majority' 
 mongoose.set('strictQuery', true);
 mongoose.connect(DB_HOTS)
 .then(() => {
