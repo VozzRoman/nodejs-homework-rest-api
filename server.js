6a1969e7aef61ee3646f90919981927934f57e89
VozzRoman <looper0500@ukr.net>
2023-04-14 18:24:42 +0300

    adding

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
-const {DB_HOTS} = require('./config');
+const {DB_HOTS} = process.env;
 
 mongoose.set('strictQuery', true);
 mongoose.connect(DB_HOTS)
