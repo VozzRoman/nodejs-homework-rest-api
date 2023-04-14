4482a8c2b2d8e8558c259583fc48b1784edc8d34
VozzRoman <looper0500@ukr.net>
2023-04-14 14:19:34 +0300

    adding

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const mongoose = require('mongoose');
+
+const app = require('./app');
+
+
+const DB_HOTS ='mongodb+srv://Roman:rKBWkc4QPlDtBiWT@cluster0.jwv1esx.mongodb.net/contacts__reader?retryWrites=true&w=majority' 
+mongoose.set('strictQuery', true);
+mongoose.connect(DB_HOTS)
+.then(() => {
+	app.listen(3000)
 })
+.catch(error => {
+	console.log(error.message);
+	process.exit(1);
+})
+
+
+
+
